81e3b51ed44d0fa198fea92a8684a8a6bc8a1f75
udrus <ionsurdu86ro@gmail.com>
2021-11-26 20:55:12 +0100

    Eu am adaugat un comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,6 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
+    // comentariu pentru tortoise
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
