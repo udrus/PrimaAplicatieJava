fd6cf654ad2fac4a1cbf9511f66c54b50c21d713
udrus <ionsurdu86ro@gmail.com>
2021-11-27 21:22:54 +0100

    Adaugara comentarii pe o singura linie

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,7 +2,10 @@ package ro.java.ctrln;
 
 public class Main {
     // comentariu pentru tortoise
+    // Aceasta e metoda main in aplicatia Java.
     public static void main(String[] args) {
+
+        // Aici vrem sa afisam pe consola IntellIJ mesajul de mai jos sau un mesaj ales de noi.
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
